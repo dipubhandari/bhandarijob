f73e55e881fc7f26abacffdfb6ee0fdbe5a814e6
dipubhandari <dipendrabhandari123456.com>
2023-01-22 12:15:03 +0545

    header design

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
-
+import './Header.css'
+import { Link } from 'react-router-dom'
+import { AiFillHome } from 'react-icons/ai'
+import { FaHandshake } from 'react-icons/fa'
+import { FaBloggerB } from 'react-icons/fa'
+import { MdPermContactCalendar } from 'react-icons/md'
 const Header = () => {
   return (
     <div className='header_container'>
 
 
       <section className="header_logo">
-        <h1>Bhandari <span>Job.</span> </h1>
+        <h1 className='logo'>Bhandari <span className='logo_special'>Job.</span> </h1>
       </section>
 
       <section className="header__menu">
+
+        <Link className="link_row">
+          <b><AiFillHome /></b> <Link className='linkheader'>Home</Link>
+        </Link>
+
+        <Link className="link_row">
+          <b><FaHandshake /></b> <Link className='linkheader'>Services</Link>
+        </Link>
+
+        <Link className="link_row">
+          <b><MdPermContactCalendar /></b> <Link className='linkheader'>Contact</Link>
+        </Link>
+
+        <Link className="link_row">
+          <b><FaBloggerB /></b> <Link className='linkheader'>Blog</Link>
+        </Link>
 
       </section>
       <section className="header_operator">

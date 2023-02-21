bb5cd5c9d1c09d8910d76d3ce4a92ac8d3554004
dipubhandari <dipendrabhandari123456@gmail.com>
2023-02-20 18:57:25 -0800

    delete resume

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(process.cwd(), "./build/index.html"))
 })
 
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(process.cwd(), "./build/index.html"))
 })
